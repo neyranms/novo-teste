fe5f7b3d98f68eaa15a611c01d8f30c3562bd7fa
Neyran Melo <neyranms>
2022-07-31 00:17:54 -0300

    teste de link

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,10 @@ import Link from 'next/link';
 
 //useState importa um componente com memória(estado), nesse caso o contador
 function Home() {
-    return (
-    <div>
-        <h1>S2</h1>
+    return (<div>
+        <h1>Home</h1>
         <Link href="/sobre">
-            <a>atualizado 11:38</a>
+            <a>Acessar a página Sobre</a>
         </Link>
        </div>
     )
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,13 +1,15 @@
+import Link from 'next/link';
+
+//useState importa um componente com memória(estado), nesse caso o contador
 function Sobre() {
-    return (
-    <div>
-        <h1>Sobre</h1>
-
-
-        <Link href="/sobre">
-            <a>Acessar página Home</a>
+    return (<div>
+        <h1>Home</h1>
+        <Link href="/">
+            <a>Acessar a página Home</a>
         </Link>
-
        </div>
     )
 }
+
+//mostra na pagina o componente Home
+export default Sobre; // Exporta a função Home
