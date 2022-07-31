0cb240f09d2e3e13f59244ca7b483e27b0c13795
Neyran Melo <neyranms>
2022-07-31 00:00:27 -0300

    updateSobre

---
+++ pages/sobre.js
@@ -0,0 +1,13 @@
+function Sobre() {
+    return (
+    <div>
+        <h1>Sobre</h1>
+
+
+        <Link href="/sobre">
+            <a>Acessar página Home</a>
+        </Link>
+
+       </div>
+    )
+}
