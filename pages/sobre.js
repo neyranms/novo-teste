39d068f97afa12df191c6b9513641857bf22c31c
Neyran Melo <neyranms>
2022-07-31 12:34:12 -0300

    update10

--- pages/sobre.js
+++ pages/sobre.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 //useState importa um componente com memória(estado), nesse caso o contador
 function Sobre() {
     return (<div>
-        <h1>Porque você clicou, Aryane?</h1>
+        <h1>Porque você clicou, Benedita?</h1>
         <Link href="/">
             <a>agora, clique aqui</a>
         </Link>
