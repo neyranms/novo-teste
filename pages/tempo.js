bc3d3b376d336a6d0a957101bbafdbf872b31098
Neyran Melo <neyranms>
2022-07-31 01:25:07 -0300

    update5

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,8 +1,28 @@
-function tempo(request, response) {
+function Tempo(props) {
+    console.log('>Passando pelo Frontend;>');
     const dynamicDate = new Date();
+    const dynamicDateString = dynamicDate.toGMTString();
 
-    response.json({
-        date: dynamicDate.toGMTString()
-    })
+    return (
+        <div>
+            <div>{dynamicDateString} (dinâmico)</div>
+            <div>{props.staticDateString} (estático)</div>
+        </div>
+    )
 }
-export default tempo;
+
+export async function getStaticProps() {
+    console.log('>Passando pelo getStaticProps;>');
+    console.log('>Adicionando delay de 5 segundos;>');
+
+    await DelayNode(5000);
+    const staticDate = new Date();
+    const staticDateString = staticDate.toGMTString();
+
+    return {
+        props: {
+            staticDateString
+        }
+    }
+}
+export default Tempo;
